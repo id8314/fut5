b15f692bc5f80f79038e90dbf17bc24469d5493b
id8314 <id8314@alunos.uminho.pt>
2022-12-14 16:39:54 +0000

    NuGet packages upgrade to latest SUPPORTED versions

--- Program.cs
+++ Program.cs
@@ -10,12 +10,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
+// Load api url (static Fut5Config.API_URL) from configured value in wwwroot/appsettings.json
 // See appsettings.json https://stackoverflow.com/a/70408216/251674
 var settings = new Fut5Config();
 builder.Configuration.Bind(settings);
 builder.Services.AddSingleton(settings);
-
 builder.Services.AddSingleton<Fut5ApiService>();
 
 await builder.Build().RunAsync();
